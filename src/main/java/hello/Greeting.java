3ac7755f599480228ab2c1c1a48221172a76bac1
Ronisko <koripier@gmail.com>
2015-10-17 14:33:00 +0200

    initial commit

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,22 @@
+package hello;
+
+/**
+ * Created by komputerszatana on 25.08.15.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
